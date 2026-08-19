2aee94cdccae25ffc364b1d367299eff6d7a69f7
forgetzz <forget.noxa90@gmail.com>
2026-08-19 17:15:23 +0800

    first commit[A

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-  apiKey: "AIzaSyCEsj7UBVRJ5FUWfxQlqcsUAqPeP4UL5U0",
-  authDomain: "nekoride-cf176.firebaseapp.com",
-  projectId: "nekoride-cf176",
-  storageBucket: "nekoride-cf176.firebasestorage.app",
-  messagingSenderId: "695899911539",
-  appId: "1:695899911539:web:e6cab1329a2b8d71fe4106",
-  measurementId: "G-Z52Y1Z58ZE"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 
