40fdc55c2f1ab04bf969380df0b0c766f6a69f4c
forgetzz <forget.noxa90@gmail.com>
2026-08-20 07:40:03 +0800

    configurasi firebase

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -5,14 +5,6 @@ import { getFirestore } from "firebase-admin/firestore";
 const projectId = process.env.FIREBASE_PROJECT_ID;
 const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
 const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n");
-console.log("Firebase Admin config:", {
-  projectId: !!projectId,
-  clientEmail: !!clientEmail,
-  privateKey: !!privateKey,
-});
-if (!projectId || !clientEmail || !privateKey) {
-  throw new Error("Firebase Admin environment variables are missing");
-}
 
 
 const firebaseAdmin: App =
